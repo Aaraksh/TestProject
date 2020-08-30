e50a7fb69c2c80a2da89ce4f09b77edd93294819
Aaraksh <aaraksh1998@gmail.com>
2020-08-30 12:12:27 +0530

    My first commit from eclipse

---
+++ DemoGitPro/src/java_prg/Sum.java
@@ -0,0 +1,22 @@
+package java_prg;
+
+import java.util.Scanner;
+
+public class Sum
+{
+
+	public static void main(String[] args) 
+	{
+		int a,b,sum;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of a: " );
+		a=sc.nextInt();
+		System.out.println("Enter the value of b: ");
+		b=sc.nextInt();
+		sum=a+b;
+		System.out.println("Sum of two numbers: "+sum);
+		sc.close();
+
+	}
+
+}
